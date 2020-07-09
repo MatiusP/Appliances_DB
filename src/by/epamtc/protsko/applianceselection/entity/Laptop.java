029d02fd0740d929338233bb73dbdfe89abdfde7
MatiusP <45049910+MatiusP@users.noreply.github.com>
2020-07-09 22:30:37 +0300

    Add equals and hashCode

--- src/by/epamtc/protsko/applianceselection/entity/Laptop.java
+++ src/by/epamtc/protsko/applianceselection/entity/Laptop.java
@@ -28,48 +28,24 @@ public class Laptop implements Appliance {
         return batteryCapacity;
     }
 
-    public void setBatteryCapacity(double batteryCapacity) {
-        this.batteryCapacity = batteryCapacity;
-    }
-
     public String getOperationSystem() {
         return operationSystem;
-    }
-
-    public void setOperationSystem(String operationSystem) {
-        this.operationSystem = operationSystem;
     }
 
     public double getMemoryRom() {
         return memoryRom;
     }
 
-    public void setMemoryRom(double memoryRom) {
-        this.memoryRom = memoryRom;
-    }
-
     public double getSystemMemory() {
         return systemMemory;
-    }
-
-    public void setSystemMemory(double systemMemory) {
-        this.systemMemory = systemMemory;
     }
 
     public double getCpu() {
         return cpu;
     }
 
-    public void setCpu(double cpu) {
-        this.cpu = cpu;
-    }
-
     public double getDisplayInches() {
         return displayInches;
-    }
-
-    public void setDisplayInches(double displayInches) {
-        this.displayInches = displayInches;
     }
 
     @Override
@@ -82,5 +58,38 @@ public class Laptop implements Appliance {
                 ", CPU=" + cpu +
                 ", Display_Inches=" + displayInches +
                 ';';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Laptop)) return false;
+
+        Laptop laptop = (Laptop) o;
+
+        if (Double.compare(laptop.batteryCapacity, batteryCapacity) != 0) return false;
+        if (Double.compare(laptop.memoryRom, memoryRom) != 0) return false;
+        if (Double.compare(laptop.systemMemory, systemMemory) != 0) return false;
+        if (Double.compare(laptop.cpu, cpu) != 0) return false;
+        if (Double.compare(laptop.displayInches, displayInches) != 0) return false;
+        return operationSystem != null ? operationSystem.equals(laptop.operationSystem) : laptop.operationSystem == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result;
+        long temp;
+        temp = Double.doubleToLongBits(batteryCapacity);
+        result = (int) (temp ^ (temp >>> 32));
+        result = 31 * result + (operationSystem != null ? operationSystem.hashCode() : 0);
+        temp = Double.doubleToLongBits(memoryRom);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        temp = Double.doubleToLongBits(systemMemory);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        temp = Double.doubleToLongBits(cpu);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        temp = Double.doubleToLongBits(displayInches);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        return result;
     }
 }
--- src/by/epamtc/protsko/applianceselection/entity/Oven.java
+++ src/by/epamtc/protsko/applianceselection/entity/Oven.java
@@ -27,48 +27,24 @@ public class Oven implements Appliance {
         return powerConsumption;
     }
 
-    public void setPowerConsumption(double powerConsumption) {
-        this.powerConsumption = powerConsumption;
-    }
-
     public double getWeight() {
         return weight;
-    }
-
-    public void setWeight(double weight) {
-        this.weight = weight;
     }
 
     public double getCapacity() {
         return capacity;
     }
 
-    public void setCapacity(double capacity) {
-        this.capacity = capacity;
-    }
-
     public double getDepth() {
         return depth;
-    }
-
-    public void setDepth(double depth) {
-        this.depth = depth;
     }
 
     public double getHeight() {
         return height;
     }
 
-    public void setHeight(double height) {
-        this.height = height;
-    }
-
     public double getWidth() {
         return width;
-    }
-
-    public void setWidth(double width) {
-        this.width = width;
     }
 
     @Override
@@ -81,5 +57,39 @@ public class Oven implements Appliance {
                 ", Height=" + height +
                 ", Width=" + width +
                 ';';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Oven)) return false;
+
+        Oven oven = (Oven) o;
+
+        if (Double.compare(oven.powerConsumption, powerConsumption) != 0) return false;
+        if (Double.compare(oven.weight, weight) != 0) return false;
+        if (Double.compare(oven.capacity, capacity) != 0) return false;
+        if (Double.compare(oven.depth, depth) != 0) return false;
+        if (Double.compare(oven.height, height) != 0) return false;
+        return Double.compare(oven.width, width) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        int result;
+        long temp;
+        temp = Double.doubleToLongBits(powerConsumption);
+        result = (int) (temp ^ (temp >>> 32));
+        temp = Double.doubleToLongBits(weight);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        temp = Double.doubleToLongBits(capacity);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        temp = Double.doubleToLongBits(depth);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        temp = Double.doubleToLongBits(height);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        temp = Double.doubleToLongBits(width);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        return result;
     }
 }
